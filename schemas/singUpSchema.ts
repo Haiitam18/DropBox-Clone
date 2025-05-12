75ffbbd7864d28ba0b4d524b776085f41c16cab0
Haitam <haitam47200@gmail.com>
2025-05-12 13:34:38 +0200

    Setup SingUp Form, HeroUI Comp, Setup Zod verif

--- schemas/singUpSchema.ts
+++ schemas/singUpSchema.ts
@@ -11,11 +11,11 @@ export const signUpSchema = z
             .string()
             .min(1, { message: 'Password is required' })
             .min(8, { message: 'Password must be at least 8 characters long' }),
-        confirmPassword : z
+        passwordConfirmation : z
             .string()
             .min(1, { message: 'Please confirm your password' })
 })
-.refine((data) => data.password === data.confirmPassword, {
+.refine((data) => data.password === data.passwordConfirmation, {
     message: 'Passwords do not match',
-    path: ['confirmPassword'],
+    path: ['passwordConfirmation'],
 })
